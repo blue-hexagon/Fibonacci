09c6565f4025f9b7a7469c137df40cfaf6665457
blue-hexagon <w0j8uhv5csio@opayq.net>
2022-10-12 14:49:44 +0200

    Fix indentation

--- src/fibonacci/fib-gen.sh
+++ src/fibonacci/fib-gen.sh
@@ -9,11 +9,10 @@ read -p "Fibonacci Sequence Length:" numberOfIterations
 n1=1
 n2=2
 iter=0
-while [[ ${iter} -le ${numberOfIterations} ]]
-do
-echo "Fib(${iter})=${n1}"
-((iter++))
-((new_number=n1+n2))
-((n1=n2))
-((n2=new_number))
+while [[ ${iter} -le ${numberOfIterations} ]]; do
+  echo "Fib(${iter})=${n1}"
+  ((iter++))
+  ((new_number = n1 + n2))
+  ((n1 = n2))
+  ((n2 = new_number))
 done
